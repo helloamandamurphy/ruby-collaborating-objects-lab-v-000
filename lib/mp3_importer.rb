72b64b54720d0c3395909391442dfd5b19dabf6a
Amanda Murphy <helloamandamurphy@gmail.com>
2018-11-05 18:33:42 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,13 @@
 class MP3Importer
-  attr_accessor :path, :filenames
+  
+  attr_accessor :path
+  
   def initialize(path)
     @path = path
   end
 
   def files
     Dir.glob("#{@path}/*.mp3").collect do |file|
-      #binding.pry
       file.gsub("#{@path}/","")
     end
   end
