e037d586c6b0a7f3c0d02b51ddbe76b328b4efd9
Amanda Murphy <helloamandamurphy@gmail.com>
2018-11-05 17:59:43 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,11 +2,10 @@ class MP3Importer
   attr_accessor :path, :filenames
   def initialize(path)
     @path = path
-    @filenames = []
   end
 
   def files
-    Dir.new(@path)
+    Dir.glob(@path)
   end
 
   def import(filenames)
